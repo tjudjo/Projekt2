ab0649ba6ae0ee38e3def8948a046a90978009b7
tjudjo <tjudjo@gmail.com>
2014-09-08 16:54:06 +0200

    ASDF

---
+++ M/src/main/java/Macierze/M/App.java
@@ -0,0 +1,13 @@
+package Macierze.M;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "dddddddddddddld" );
+    }
+}
